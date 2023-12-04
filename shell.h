194a14d3d18f485f5270804e43299ff48afacaf5
Kojo <gideonad4@gmail.com>
2023-12-04 09:37:13 -0800

    added to shell.h

--- shell.h
+++ shell.h
@@ -1,8 +1,12 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
+char **split_string(char *str, char *delim);
 #endif /* SHELL_H */
