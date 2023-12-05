a1a1105c5d93508e5e2e9ed1c8be3dc94650ba7e
Kojo <gideonad4@gmail.com>
2023-12-05 06:39:45 -0800

    modified shell.h

--- shell.h
+++ shell.h
@@ -5,8 +5,10 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdio.h>
+#include <sys/stat.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/sysmacros>
 
 extern char **environ;
 char **split_string(char *str, size_t delim);
