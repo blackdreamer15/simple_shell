060b1d5cc30cac9d4251b8c06ed1e7374f2d5a93
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-12-12 02:38:37 +0000

    Update shell.c

--- shell.c
+++ shell.c
@@ -6,44 +6,23 @@
  * @av: arguments passed
  * Return: 0 on success
  */
-int main(int ac, char *av[])
+int main(void)
 {
-	char **argv;
-	char *cmd = NULL; /* initialize to NULL*/
-	size_t len = 0;	  /* initialize to 0*/
-	ssize_t nread;
-	pid_t child_pid;
-	int status;
+	char user_command[127];
+	char *commands[10];
+	int num_commands;
+	int i;
 
-	if (ac > 1)
+	while (1)
 	{
-		child_pid = fork();
-		execute_cmd(av, child_pid, &status, 0);
-	}
+		display_prompt();
+		read_input(user_command, sizeof(user_command));
+		num_commands = split_commands(user_command, commands);
 
-	prompt(0);
-
-	while ((nread = getline(&cmd, &len, stdin)) != -1)
-	{
-		if (*cmd == '\n')
+		for (i = 0; i < num_commands; ++i)
 		{
-			prompt(0);
-			continue;
+			process_command(commands[i]);
 		}
-
-		argv = split_string(cmd); /*take command to argv*/
-		child_pid = fork();
-
-		if (child_pid == -1)
-			continue;
-
-		execute_cmd(argv, child_pid, &status, 1);
-		prompt(0);
-		free(cmd);
-		free_av(argv);
-		cmd = 0;
-		len = 0; /* reset to 0*/
 	}
-	free(cmd);
 	return (0);
 }
