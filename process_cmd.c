ff8a19ff5ec4f8cf266a71997c9d41c3d869b6fa
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-12-12 02:55:11 +0000

    Create process_cmd.c

---
+++ process_cmd.c
@@ -0,0 +1,30 @@
+#include "shell.h"
+
+/**
+ * process_command - Processes individual shell commands
+ * @command: The command to be processed
+ */
+
+void process_command(char *command)
+{
+	if (my_strncmp(command, "exit", 4) == 0)
+		handle_exit(command);
+	
+	else if (my_strncmp(command, "env", 3) == 0)
+		print_environment();
+	
+	else if (my_strncmp(command, "setenv", 6) == 0)
+		handle_setenv(command);
+	
+	else if (my_strncmp(command, "unsetenv", 8) == 0)
+		handle_unsetenv(command);
+	
+	else if (my_strncmp(command, "cd", 2) == 0)
+	{
+		char *path = my_strtok(command + 3, " ");
+
+		change_directory(path);
+	}
+	else
+		execute_prompt(command);
+}
