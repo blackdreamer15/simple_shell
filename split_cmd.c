557512b2089973d8ccd89db472ad9d664f8447ca
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-12-12 03:51:51 +0000

    Update split_cmd.c

--- split_cmd.c
+++ split_cmd.c
@@ -3,18 +3,17 @@
 /**
  * split_commands - Splits input commands based on the ";" delimiter
  * @input: The input string containing commands separated by ";"
- * @commands: An array to store the individual commands
+ * @cmds: Array of individual commands
  * Return: The number of commands
  */
-
-int split_commands(char *input, char *commands[])
+int split_commands(char *input, char *cmds[])
 {
 	int cmd_count = 0;
 	char *token = strtok(input, ";");
 
 	while (token != NULL && cmd_count < 10)
 	{
-		commands[cmd_count++] = token;
+		cmds[cmd_count++] = token;
 		token = strtok(NULL, ";");
 	}
 	return (cmd_count);
