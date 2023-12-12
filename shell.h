8e370e3324854535d17a0154b602c96128283a53
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-12-12 02:30:38 +0000

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,24 +1,61 @@
 #ifndef SHELL_H
 #define SHELL_H
+#ifndef PATH_MAX
+#define PATH_MAX 4096
+#endif
 
+#define BUFFER_SIZE 1024
+#define MAX_ARGS 170
+
+/**
+ * This header file contains all the functions
+ * of the simple-shell project.
+ * It also contains all the relevant header files from
+ * the standard library.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <stdio.h>
-#include <sys/stat.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/sysmacros.h>
-
-#define MAX_INPUT 1024
-#define MAX_ARGS 64
+#include <stdarg.h>
+#include <errno.h>
+#include <stddef.h>
+#include <limits.h>
 
 extern char **environ;
 
-int _putchar(char c);
-int prompt(char *c);
-char **split_string(char *str);
-void free_av(char **av);
-int execute_cmd(char **av, pid_t pid, int *status, int flag);
+
+int add_numbers(int nma, int mpa);
+int main(void);
+void display_prompt(void);
+void read_input(char *input, size_t size_of_input);
+void execute_prompt(const char *command);
+void execute_command_with_args(const char *command);
+void execute_command(const char *command);
+int is_command_in_path(const char *command);
+void exit_shell(int status);
+void print_environment(void);
+void handle_setenv(const char *command);
+void handle_unsetenv(const char *command);
+int set_environment_variable(const char *variable, const char *value);
+int unset_environment_variable(const char *variable);
+void change_directory(const char *path);
+int split_commands(char *input, char *commands[]);
+void handle_exit(char *command);
+void process_command(char *command);
+
+
+void my_printf(const char *print_command, ...);
+ssize_t _getline(char **lineptr, size_t *n);
+char *my_strtok(char *input_str, char *delimiter);
+int my_strncmp(const char *str1, const char *str2, size_t max_chars);
+char *my_strstr(const char *stack, const char *needle);
+
+
+
 
 #endif /* SHELL_H */
