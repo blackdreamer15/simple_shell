7d178f6981999d99d835181f7cb7cd26716867ba
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-12-12 02:56:36 +0000

    Create exit_shell.c

---
+++ exit_shell.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+
+/**
+ * handle_exit - Handles the exit command
+ * @command: The exit command
+ */
+
+void handle_exit(char *command)
+{
+	int status = atoi(command + 5);
+
+	exit_shell(status);
+}
